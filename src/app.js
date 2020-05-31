1d06a8f49d7c800218d8fadcf5668fdb77d3bc66
Henrique S.A <henriqueator@gmail.com>
2020-05-31 20:06:35 -0300

    Servidor estático

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
+//-> Servidor estático.
+
+const path = require('path');
 const express = require('express');
+
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+const app = express();
+
+app.use(express.static(publicDirectoryPath));
+
+app.listen(3000, () => {
+  console.log('Server is up running');
+});
